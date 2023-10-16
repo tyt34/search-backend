b97679d6bcb7a428a65c4ce89e09f75ef9496d4d
tyt34 <novoto4in@mail.ru>
2023-10-16 23:19:39 +0300

    добавил функций в try catch

--- controllers/search.ts
+++ controllers/search.ts
@@ -10,31 +10,20 @@ import {
 export const search = async (req, res) => {
   console.log({ qSearch: req.query })
 
-  const pageNumber = Number(req.query.page)
-  const typeSearch = req.query.search_type
-
-  const arrField = arrFilter(req.query.field)
-  const textSearch = splitText(req.query.text)
-
-  const conf = {
-    text: textSearch,
-    fields: arrField,
-    page: pageNumber
-  }
-
-  const query = methodsSearch[typeSearch](conf)
-
-  // const opensearchResponse = await client.search({
-  //   index: opensearchConf.index,
-  //   body: query
-  // })
-
-  // const { body, statusCode } = opensearchResponse
-  // const resultReq = getReqResult(body)
-
-  // res.status(statusCode).send(resultReq)
-
   try {
+    const pageNumber = Number(req.query.page)
+    const typeSearch = req.query.search_type
+    const arrField = arrFilter(req.query.field)
+    const textSearch = splitText(req.query.text)
+
+    const conf = {
+      text: textSearch,
+      fields: arrField,
+      page: pageNumber
+    }
+
+    const query = methodsSearch[typeSearch](conf)
+
     const opensearchResponse = await client.search({
       index: opensearchConf.index,
       body: query
@@ -44,6 +33,6 @@ export const search = async (req, res) => {
     const resultReq = getReqResult(body)
     res.status(statusCode).send(resultReq)
   } catch (error) {
-    res.status(400).send({ error })
+    res.status(400).send({ data: [], total: 0 })
   }
 }
